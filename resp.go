a43ab6693a1183949c61277b4a5a26e9ce3f7716
Ansh Bajpai <bajpaiansh8@gmail.com>
2024-10-01 12:07:14 +0530

    Added read array & bulk capability to deserialize the values

--- resp.go
+++ resp.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"strconv"
 )
@@ -55,4 +56,72 @@ func (resp *Resp) readInteger() (number int, bytesRead int, err error) {
 		return 0, bytesRead, err
 	}
 	return int(parsedInt64), bytesRead, nil
+}
+
+func (resp *Resp) Read() (Value, error) {
+	dataType, err := resp.reader.ReadByte()
+
+	if err != nil {
+		return Value{}, err
+	}
+
+	switch dataType {
+	case ARRAY:
+		return resp.readArray()
+	case BULK:
+		return resp.readBulk()
+	default:
+		fmt.Printf("Unrecognized data type: %v", string(dataType))
+		return Value{}, nil
+	}
+}
+
+func (resp *Resp) readArray() (Value, error) {
+	result := Value{}
+	result.typ = "array"
+
+	// Read the array length
+	length, _, err := resp.readInteger()
+	if err != nil {
+		return result, err
+	}
+
+	// Initialize the array to hold the parsed values
+	result.array = make([]Value, 0)
+	for i := 0; i < length; i++ {
+		parsedValue, err := resp.Read()
+		if err != nil {
+			return result, err
+		}
+
+		// Append each parsed value to the result array
+		result.array = append(result.array, parsedValue)
+	}
+
+	return result, nil
+}
+
+func (resp *Resp) readBulk() (Value, error) {
+	result := Value{}
+	result.typ = "bulk"
+
+	// Read the length of the bulk string
+	length, _, err := resp.readInteger()
+	if err != nil {
+		return result, err
+	}
+
+	// Allocate a buffer for the bulk data
+	bulkData := make([]byte, length)
+
+	// Read the bulk data into the buffer
+	resp.reader.Read(bulkData)
+
+	// Convert the byte slice into a string
+	result.bulk = string(bulkData)
+
+	// Read and discard the trailing CRLF
+	resp.readLine()
+
+	return result, nil
 }
