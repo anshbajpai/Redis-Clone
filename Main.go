5503f8eb04fe10f7cfab98620bbe0e12faecb80f
Ansh Bajpai <bajpaiansh8@gmail.com>
2024-10-01 11:45:11 +0530

    Added infinite loop to listen to requests and respond to confirm connection

--- Main.go
+++ Main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 func main() {
-	listener, error := net.Listen("tcp", "6379")
+	listener, error := net.Listen("tcp", ":6379")
 	if error != nil {
 		fmt.Println(error)
 		return
@@ -18,4 +20,19 @@ func main() {
 	}
 
 	defer connection.Close()
+
+	for {
+		buffer := make([]byte, 1024)
+
+		_, error = connection.Read(buffer)
+		if error != nil {
+			if error == io.EOF {
+				break
+			}
+			fmt.Println("Reading error from client: ", error.Error())
+			os.Exit(1)
+		}
+
+		connection.Write([]byte("+PONG OK\r\n"))
+	}
 }
